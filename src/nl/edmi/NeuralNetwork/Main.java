5ade349fa9257c701d6bb4c152eaeb7162634c4d
Vescatur <ivan@edmi.nl>
2016-12-19 17:12:26 +0100

    Eerste lijn code

--- src/nl/edmi/NeuralNetwork/Main.java
+++ src/nl/edmi/NeuralNetwork/Main.java
@@ -2,7 +2,13 @@ package nl.edmi.NeuralNetwork;
 
 public class Main {
 
+    public Main(){
+        System.out.print("2");
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        new Main();
+ 	    System.out.println("stop");
     }
 }
+
---
+++ src/nl/edmi/NeuralNetwork/NeuralNet.java
@@ -0,0 +1,8 @@
+package nl.edmi.NeuralNetwork;
+
+/**
+ * Created by Ivan on 19/12/2016.
+ */
+public class NeuralNet {
+
+}
